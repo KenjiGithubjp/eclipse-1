d344adbc842c7b307b0a786cc0278a1b4baf7856
SunshineGitKen <kenjimaeda.888@gmail.com>
2023-03-12 22:43:35 +0900

    制作プログラムをまとめてコミットします。

---
+++ sampleProject/src/オブジェクト指向を用いた数字の大小を当てるハイローゲーム/NumberOutOfRange_Exception.java
@@ -0,0 +1,9 @@
+package オブジェクト指向を用いた数字の大小を当てるハイローゲーム;
+//Object-Oriented "High & Low" Game To Guess Number Size
+
+public class NumberOutOfRange_Exception extends Exception {
+	public NumberOutOfRange_Exception(String msg) {
+		super(msg);
+	}
+
+}
